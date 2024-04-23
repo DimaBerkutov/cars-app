fe352d3789e27f4808215c04bcc12dc1496494a0
Dima <dmytro@playboxtech.com>
2024-04-23 11:52:11 +0300

    Resizer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AppRoutingModule } from './app.routing.module';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule], // added CarsComponent to use it
+  imports: [RouterOutlet, CommonModule, AppRoutingModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
--- src/app/cars-list/cars-list.component.ts
+++ src/app/cars-list/cars-list.component.ts
@@ -1,16 +1,25 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+
 import { MatListModule } from '@angular/material/list';
+
 import { cars } from '../../data/cars';
+import { ResizeDirective } from '../../directives/resizer.directives';
 import { ICar } from '../../models/cars';
 import { CarsComponent } from '../cars/cars.component';
 
 @Component({
   selector: 'app-cars-list',
   standalone: true,
-  imports: [CarsComponent, MatListModule],
+  imports: [CommonModule, CarsComponent, MatListModule, ResizeDirective],
   templateUrl: './cars-list.component.html',
   styleUrl: './cars-list.component.scss',
 })
 export class CarsListComponent {
   cars: ICar[] = cars;
+  car: ICar = cars[1];
+
+  onCarClick(car: ICar) {
+    this.car = car;
+  }
 }
