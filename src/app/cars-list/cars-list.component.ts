4ace4ae9abdaf9b266ff0645ad2637566083230d
Dima <dmytro@playboxtech.com>
2024-04-24 12:00:27 +0300

    angular forms

--- src/app/cars-list/cars-list.component.ts
+++ src/app/cars-list/cars-list.component.ts
@@ -1,5 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import {
+  FormGroup,
+  FormControl,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 import { MatListModule } from '@angular/material/list';
 
@@ -11,13 +17,37 @@ import { CarsComponent } from '../cars/cars.component';
 @Component({
   selector: 'app-cars-list',
   standalone: true,
-  imports: [CommonModule, CarsComponent, MatListModule, ResizeDirective],
+  imports: [
+    CommonModule,
+    CarsComponent,
+    MatListModule,
+    ResizeDirective,
+    ReactiveFormsModule,
+  ],
   templateUrl: './cars-list.component.html',
   styleUrl: './cars-list.component.scss',
 })
 export class CarsListComponent {
   cars: ICar[] = cars;
   car: ICar = cars[1];
+
+  carForm = new FormGroup({
+    name: new FormControl('', Validators.required),
+    color: new FormControl('', Validators.required),
+    price: new FormControl('', Validators.required),
+    discription: new FormControl('', Validators.required),
+  });
+
+  onFormDataSave() {
+    if (this.carForm.valid) {
+      console.log('qqq', this.carForm.value);
+
+      // this.cars = cars.push({
+      //   id: cars.length,
+      //   ...this.carForm.value,
+      // });
+    }
+  }
 
   onCarClick(car: ICar) {
     this.car = car;
